77d6ac884e6ba4cd86e253f72efa4ef8f3e56069
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-04-23 18:08:21 -0500

    .env fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import TeamRosters from './components/TeamRosters'
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Dialog from 'material-ui/Dialog';
-const config = require('./config.js');
+const dotenv = require('dotenv');
+dotenv.config();
 
 class App extends Component {
     state = {
@@ -42,7 +43,7 @@ class App extends Component {
   getLeagueData = () => {
     let base_url = this.urls.en + '/leagueTable'; 
     var myHeaders = new Headers();
-    myHeaders.append("X-Auth-Token", `${config.MY_API_TOKEN}`);
+    myHeaders.append("X-Auth-Token", process.env.REACT_APP_MY_API_TOKEN);
     var myInit = {
       method: 'GET',
       headers: myHeaders,
