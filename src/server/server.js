bfd42b5da588dcd7e8afbbdf72bc67445d0fddfd
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-04-23 18:45:37 -0500

    serve api

--- src/server/server.js
+++ src/server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000, function () {
