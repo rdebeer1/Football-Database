8b69222752870ac7a46c8a35879edaa9e6cf33a0
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-04-23 23:42:26 -0500

    response header

--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const port = process.env.PORT || 3000;
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.header()
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
