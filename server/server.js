24b010128330a2e88504473006f1021ea64e38cc
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-04-23 23:46:51 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || 3000;
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
