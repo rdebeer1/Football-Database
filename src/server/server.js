6b6cc642daba934a4c97bb2ee0fbad25895e70af
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-04-23 22:33:49 -0500

    port

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 app.get('/ping', function (req, res) {
   return res.send('pong');
