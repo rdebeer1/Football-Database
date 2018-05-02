d39d44d5c5a7e262aa02273f425276d124e80c65
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-05-02 02:11:41 -0500

    deploy

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/DeBeer/projects/football-database/src/layouts/index.js"
-  import data from "/Users/DeBeer/projects/football-database/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
